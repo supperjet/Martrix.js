44f90b63e8c40e49796b1855df36bbcc523706c1
supperjet <zhouyufei1990@126.com>
2016-12-25 19:35:28 +0800

    Martrix.js

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Martrix.js
-A martrix effect plugin
+canvas黑客帝国特效插件
+### 1、Feature
+1. 使用原生canvas，不需依赖任何外部插件
+1. 支持`AMD`&&`CMD`规范
+
+### 2、Install&&Useage
+下载文件引入`martrix.js`或`martrix.min.js`文件
+
+```bash
+ <canvas id="canvas" style="background:#000">
+       your browser not support canavs
+   </canvas>
+   
+   <script>
+       martrix('canvas', {
+           cW: 1390,
+           cH: 700,
+           wordColor: '#FFFC3A',
+           fontSize: 13,
+           speed: 0.13,
+       });
+```
+
+###3、DEMO
+运行`test.html`
+
+![](Imgs/demo.png)
+
+###4、Configure
+|   属性  |   类型  | 默认值 |   描述   |
+|--------|-------- |-------|-------- |
+|cW| Number| 1367  |canvas宽度|
+|cH| Number| 700  |canvas高度|
+|wordColor| String | '#33ff33' |文字颜色|
+|fontSize| Number| 15|文字大小|
+| speed| Number | 0.13  |下落速度|
+|words| String | ”0123456...“ |文字|
+
+
+###5、Supports browser
+- IE 10+
+- Chrome
+- Firefox
+- Opera
+- Safari
---
+++ dist/martrix.js
@@ -0,0 +1,99 @@
+/*
+Author Yufei
+
+*/
+
+(function(window, factory){
+    if(typeof define === "function" && define.amd){
+	//AMD
+		define(factory);
+	}else if(typeof module === "object" && module.exports){
+	//CMD
+		module.exports = factory();
+	}else{
+	//window
+		window.martrix = factory();
+	}
+
+}(typeof window !== "undefined" ? window : this, function(){
+    var martrix = function(selector, userOptions){
+        "use strict";
+        
+        var options = {
+            cW: 1368,
+            cH: 600,
+            wordColor: '#33ff33',
+            fontSize: 16,
+            speed: 0.13,
+            words: "0123456789qwertyuiopasdfghjklzxcvbnm,./;'\[]QWERTYUIOP{}ASDFGHJHJKL:ZXCVBBNM<>?" 
+        }
+       
+        var canvas, 
+            ctx, 
+            W, 
+            H, 
+            clumns,
+            wordsArr,
+            drops = [];
+        
+        
+        //混合参数
+        var mergeOptions = function(userOptions, options){
+            Object.keys(userOptions).forEach(function(key){
+                options[key] = userOptions[key];
+            }) 
+        }
+        
+        //draw
+        var draw = function(){
+           ctx.save();
+           ctx.fillStyle = options.wordColor;
+           ctx.font = options.fontSize + "px arial";
+           ctx.fontWeight = options.fontWeight;
+           for (var i = 0; i < drops.length; i++){
+				var text = wordsArr[Math.floor(Math.random() * wordsArr.length)];
+				ctx.fillText(text, i * options.fontSize, drops[i] * options.fontSize);
+				if (drops[i] * options.fontSize > H && Math.random() > 0.98){
+                        drops[i] = 0;
+                }
+				drops[i]++;
+           }
+           ctx.restore();
+           
+       }    
+        //初始化
+        var initSetup = function(selector, userOptions){
+            
+            mergeOptions(userOptions, options);
+            
+            canvas = document.getElementById(selector);
+            ctx = canvas.getContext('2d');
+            
+            canvas.height = H = options.cH;
+            canvas.width = W = options.cW;
+            
+            clumns = options.cW / options.fontSize;
+            wordsArr = options.words.split('')
+            
+            for(var i=0; i<clumns; i++){
+                 drops[i] = 1;
+            }
+           
+            (function drawFrame(){
+                window.requestAnimationFrame(drawFrame);
+                ctx.fillStyle = 'rgba(0,0,0,'+options.speed+')';
+                ctx.fillRect(0, 0, W, H);
+                
+                draw();
+                
+            }())
+            
+        } 
+        
+        initSetup(selector, userOptions);
+        
+  }
+    
+    return martrix;
+    
+}))
